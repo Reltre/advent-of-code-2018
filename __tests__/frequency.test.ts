7e445b5aab1af8d19c7baf0816f16219409b46b4
Reltre <tglippert@gmail.com>
2018-12-01 16:46:07 -0800

    Add part 2 and tests.

---
+++ __tests__/frequency.test.ts
@@ -0,0 +1,47 @@
+import { FrequencyPtOne } from '../src/frequencyPtOne';
+import { FrequencyPtTwo } from '../src/frequencyPtTwo';
+
+namespace FrequencyPartOneTests {
+  test('it reads in an input file', () => {
+    const frequency = new FrequencyPtOne();
+    expect(frequency.data).not.toBeUndefined;
+  });
+
+  test('it keeps a running count of the frequency', () => {
+    const frequency = new FrequencyPtOne();
+    expect(typeof frequency.count).toEqual('number');
+  });
+
+  test('it updates count with data read in from input', () => {
+    const frequency = new FrequencyPtOne();
+    frequency.updateCount();
+    expect(frequency.count).not.toEqual(0);
+  })
+
+  test('computes the frequency', () => {
+    const frequency = new FrequencyPtOne();
+    frequency.updateCount();
+    expect(frequency.count).toEqual(500);
+  })
+}
+
+namespace FrequencyPartTwoTests {
+  test('it formats data as an array', () => {
+    const frequency = new FrequencyPtTwo();
+    expect((frequency.data instanceof Array)).toBe(true);
+  })
+
+  test('it keeps a record of past frequencies', () => {
+    const frequency = new FrequencyPtTwo();
+    frequency.data = [3, 3, 4, -2, -4];
+    frequency.calibrate();
+    expect(frequency.history).not.toEqual([]);
+  })
+
+  test('it calibrates the frequency based on duplicate values', () => {
+    const frequency = new FrequencyPtTwo();
+    frequency.data = [3, 3, 4, -2, -4];
+    frequency.calibrate();
+    expect(frequency.count).toEqual(10);
+  }) 
+}
---
+++ src/frequencyPtTwo.ts
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+export class FrequencyPtTwo {
+  rawData: string | undefined
+  data: Array<number>;
+  count: number;
+  history: Array<number>;
+
+  constructor() {
+    this.rawData = fs.readFileSync('input').toString();
+    this.count = 0;
+    this.postInitialize();
+    this.history = [];
+  }
+
+  postInitialize() {
+    this.data = this.rawData.split('\n').map( string => Number(string));
+  }
+
+  calibrate(): number | undefined {
+    let currentNumber = 0;
+    let frequencyExists = false;
+    const counter = this.nextData();
+
+    while(true) {
+      currentNumber = counter.next().value;
+      this.count += currentNumber;
+      frequencyExists = this.checkHistory();
+      if(frequencyExists) return this.count;
+      this.addToHistory();
+    }
+  }
+
+  checkHistory() {
+    return !!this.history.find((currentValue) => currentValue == this.count)
+  }
+
+  addToHistory() {
+    this.history.push(this.count);
+  }
+
+  *nextData():IterableIterator<number> {
+    let count = 0;
+    while(true) {
+      yield this.data[count % this.data.length];
+      count += 1;
+    }
+  }
+}
