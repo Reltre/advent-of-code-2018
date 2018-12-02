3f936c904c3e67a658a62c5ed92f2982ffaa6d1f
Reltre <tglippert@gmail.com>
2018-12-01 16:45:58 -0800

    Implement part 1.

---
+++ src/frequencyPtOne.ts
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+export class FrequencyPtOne {
+  rawData: string | undefined
+  data: Array<number>;
+  count: number;
+
+  constructor() {
+    this.rawData = fs.readFileSync('input').toString();
+    this.count = 0;
+  }
+
+  updateCount() {
+    this.rawData.split("\n").forEach((numberString) => {
+      this.count += Number(numberString);
+    })
+  }
+}
+
+// const f = new Frequency();
+// f.updateCount();
+// console.log(f.count);
