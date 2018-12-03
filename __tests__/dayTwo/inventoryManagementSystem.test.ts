b763375f7d78cc31c6bbc4f92bbfd970d1907bae
Reltre <tglippert@gmail.com>
2018-12-02 18:07:07 -0800

    Parts 1 and 2 for day 2 task.

--- __tests__/dayTwo/inventoryManagementSystem.test.ts
+++ __tests__/dayTwo/inventoryManagementSystem.test.ts
@@ -1,9 +1,36 @@
-namespace InventoryManagementSystemTestsPtOne {
-  test('placeholder', () => {
+import InventorySystem from '../../src/dayTwo/inventoryManagementSystem'
+import { notDeepEqual } from 'assert';
 
+namespace InventoryManagementSystemTestsPtOne {
+  test('reads in an input file', () => {
+    const inventory = new InventorySystem()
+    expect(typeof inventory.data).toBe('string');
   })
 
+  test('keeps a count of IDs with pairs', () => {
+    const inventory = new InventorySystem()
+    expect(inventory.doubleCount).not.toBe(undefined);
+  })
 
+  test('keeps a count of IDs with triple values', () => {
+    const inventory = new InventorySystem()
+    expect(inventory.tripleCount).not.toBe(undefined);
+  })
+
+  test('computes a checksum', () => {
+    const inventory = new InventorySystem();
+    inventory.computeCheckSum();
+    expect(inventory.doubleCount).toBeGreaterThan(0);
+    expect(inventory.tripleCount).toBeGreaterThan(0);
+    expect(inventory.checkSum)
+      .toEqual(inventory.doubleCount * inventory.tripleCount);
+  })
+
+  test('finds ids that differ by one character', () => {
+    const inventory = new InventorySystem();
+    inventory.data = "fghij\nfguij"
+    expect(inventory.commonIdValues()).toEqual('fgij')
+  })
 }
 
 namespace InventoryManagementSystemTestsPtTwo {
---
+++ src/dayTwo/inventoryManagementSystem.ts
@@ -0,0 +1,69 @@
+const fs = require('fs');
+export default class InventorySystem {
+  data: string | undefined;
+  doubleCount: number | undefined;
+  tripleCount: number | undefined;
+  checkSum: number | undefined;
+
+  constructor() {
+    this.data = fs.readFileSync('src/dayTwo/input').toString();
+    this.doubleCount = 0;
+    this.tripleCount = 0;
+  }
+
+  computeCheckSum() {
+    this.data.split("\n").forEach((id) => {
+      const double = this.findCountOf(id, 2);
+      const triple = this.findCountOf(id, 3);
+      if(double) this.doubleCount += 1 
+      if(triple) this.tripleCount += 1
+    });
+    this.checkSum = Number(this.doubleCount) * Number(this.tripleCount);
+  }
+
+  findCountOf(id, number) {
+    let match = null;
+    for(let char of id) {
+      match = id.match(new RegExp(char, 'g'));
+      if (match.length === number) return match[0];
+    }
+    return undefined;
+  }
+
+  commonIdValues() {
+    const entries = this.data.split("\n");
+    let differentCharacterIndex = -1;
+    let commonCharacters = '';
+    entries.forEach((outerId) => {
+      entries.slice(1).forEach((innerId) => {
+        differentCharacterIndex = this.findDifferentCharacterIndex(innerId, outerId);
+        if(differentCharacterIndex) {
+          commonCharacters = innerId.slice(0, differentCharacterIndex)
+                                    .concat(innerId.slice(differentCharacterIndex + 1));
+        }
+      })
+    });
+    return commonCharacters;
+  }
+
+  findDifferentCharacterIndex(idOne, idTwo) {
+    let differenceCount = 0;
+    let differentCharacterIndex = NaN;
+    for(let index = 0; index < idOne.length; index += 1) {
+      if(idOne[index] !== idTwo[index]) {
+        differenceCount += 1;
+      }
+      if(differenceCount > 1) {
+        differentCharacterIndex = NaN;
+        break;
+      }
+      if(differenceCount == 1 && !differentCharacterIndex) {
+        differentCharacterIndex = index;
+      }
+    }
+    return differentCharacterIndex;
+  }
+}
+
+const inventory = new InventorySystem();
+console.log(inventory.commonIdValues());
